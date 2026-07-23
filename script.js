e3f358098712a9db8d273a7b289da7a32b1c58d6
Shams Kabir <kabir.shams.2002@gmail.com>
2026-07-23 20:27:50 +0600

    Add Codeforces icon URL to script.js

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ const CUSTOM_ICON_MAP = {
     "claude.ai": "https://img.icons8.com/?size=100&id=zQjzFjPpT2Ek&format=png&color=000000",
     "www.leetcode.com": "https://img.icons8.com/?size=100&id=wDGo581Ea5Nf&format=png&color=000000",
     "www.evernote.com": "https://img.icons8.com/?size=100&id=HsV0BZAmh5Qy&format=png&color=000000",
+    "www.codeforces.com": "https://img.icons8.com/?size=100&id=jldAN67IAsrW&format=png&color=000000",
 };
 
 // In-memory tray bookmark store; populated from localStorage or Firestore.
